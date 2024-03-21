7932397fb300c6f0d735066773542dc6d471826f
Hashim34643 <hashimhussain7312@gmail.com>
2024-03-21 03:51:46 +0000

    sign up component modification

--- StreamFlow/src/components/Sign-up.jsx
+++ StreamFlow/src/components/Sign-up.jsx
@@ -62,29 +62,30 @@ const SignUpForm = () => {
                 {successMessage && <div className="success-message">{successMessage}</div>}
                 {errorMessage && <div className="error-message">{errorMessage}</div>}
                 <div className="form-group">
-                    <label htmlFor='username'>Username:</label>
-                    <input type="text" id='username' name='username' value={formData.username} onChange={handleChange} />
+                    <label htmlFor='username'>Username:<span className="required-asterisk">*</span></label>
+                    <input type="text" id='username' name='username' value={formData.username} onChange={handleChange} required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor='firstName'>First Name:</label>
-                    <input type="text" id='firstName' name='firstName' value={formData.firstName} onChange={handleChange} />
+                    <label htmlFor='firstName'>First Name:<span className="required-asterisk">*</span></label>
+                    <input type="text" id='firstName' name='firstName' value={formData.firstName} onChange={handleChange} required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor='lastName'>Last Name:</label>
-                    <input type="text" id='lastName' name='lastName' value={formData.lastName} onChange={handleChange} />
+                    <label htmlFor='lastName'>Last Name:<span className="required-asterisk">*</span></label>
+                    <input type="text" id='lastName' name='lastName' value={formData.lastName} onChange={handleChange} required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor='email'>Email:</label>
-                    <input type="email" id='email' name='email' value={formData.email} onChange={handleChange} />
+                    <label htmlFor='email'>Email:<span className="required-asterisk">*</span></label>
+                    <input type="email" id='email' name='email' value={formData.email} onChange={handleChange} required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor='password'>Password:</label>
-                    <input type="password" id='password' name='password' value={formData.password} onChange={handleChange} />
+                    <label htmlFor='password'>Password:<span className="required-asterisk">*</span></label>
+                    <input type="password" id='password' name='password' value={formData.password} onChange={handleChange} required />
                 </div>
                 <div className="form-group">
-                    <label htmlFor='confirmPassword'>Confirm Password:</label>
-                    <input type="password" id='confirmPassword' name='confirmPassword' value={formData.confirmPassword} onChange={handleChange} />
+                    <label htmlFor='confirmPassword'>Confirm Password:<span className="required-asterisk">*</span></label>
+                    <input type="password" id='confirmPassword' name='confirmPassword' value={formData.confirmPassword} onChange={handleChange} required />
                 </div>
+
                 <div className="form-group switch-container">
                     <span>Streamer?</span>
                     <label className="switch">

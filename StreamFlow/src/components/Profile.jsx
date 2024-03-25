1f3a8591279668ea83c428143aae7a84f5d7f487
Hashim34643 <hashimhussain7312@gmail.com>
2024-03-25 03:05:02 +0000

    profile styling

--- StreamFlow/src/components/Profile.jsx
+++ StreamFlow/src/components/Profile.jsx
@@ -9,26 +9,32 @@ const Profile = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
+        document.body.classList.add('body-profile');
+    
         const fetchUserData = async () => {
             const config = {
                 headers: {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
             };
-
+    
             try {
                 const userResponse = await axios.get('https://streamflow-backend.onrender.com/profile', config);
                 setUser(userResponse.data.user);
-
+    
                 const streamsResponse = await axios.get(`https://streamflow-backend.onrender.com/${userResponse.data.user._id}/streams`, config);
                 setRecentStreams(streamsResponse.data.streams);
             } catch (error) {
                 console.error("Failed to fetch user data or streams", error);
             }
         };
-
+    
         fetchUserData();
+            return () => {
+            document.body.classList.remove('body-profile');
+        };
     }, []);
+    
 
     const handleLogout = () => {
         localStorage.removeItem('token');

ec35b3232a0a1afbb1605b2b3eefcf51db2b87a8
Hashim34643 <hashimhussain7312@gmail.com>
2024-04-04 15:16:53 +0100

    search bar updated

--- StreamFlow/src/components/Home.jsx
+++ StreamFlow/src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Header';
 import SearchComponent from './Search';
-import SearchResults from './Search-results';
 
 const Home = () => {
   return (

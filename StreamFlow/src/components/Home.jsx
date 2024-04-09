bca956a4997c02c10e8b6252e214c580fd8ddaa6
Hashim34643 <hashimhussain7312@gmail.com>
2024-04-09 17:33:56 +0100

    mounted start stream into home

--- StreamFlow/src/components/Home.jsx
+++ StreamFlow/src/components/Home.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import SearchComponent from './Search';
 import StartStream from './Start-stream';
 
 const Home = () => {
+  const [showStartStream, setShowStartStream] = useState(false);
+
   return (
     <div>
       <Header />
       <SearchComponent />
-      <StartStream />
+      <button onClick={() => setShowStartStream(true)} style={{margin: "20px", padding: "10px"}}>
+        Start Streaming
+      </button>
+      {showStartStream && <StartStream />}
     </div>
   );
 };
 
 export default Home;
+
